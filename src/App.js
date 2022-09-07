538cf8206e86719e25337fd72944f3fdbecb5078
Bebek-hub <71845179+Bebek-hub@users.noreply.github.com>
2022-09-07 15:56:25 +0100

    Edited the "App.js" file

--- src/App.js
+++ src/App.js
@@ -5,31 +5,36 @@ import Screen from './Calculator/Screen';
 import ButtonBg from './Calculator/ButtonBg';
 import Button from './Calculator/Button';
 
-// let value = [1,2,3];
+let btnValues = [
+  ["AC", "DEL", "/"],
+  [7, 8, 9, "*"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="]
+];
 
 function App() {
   return (
     <Container>
       <Screen />
       <ButtonBg>
-        <Button className="operator" value="AC" />
-        <Button className="operator" value="DEL" />
-        <Button className="operator" value="/" />
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
-        <Button className="operator" value="*" />
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
-        <Button className="operator" value="+" />
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
-        <Button className="operator" value="-" />
-        <Button value="0" />
-        <Button value="." />
-        <Button className="operator" value="=" />
+          {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button 
+              key={i}
+              value={btn}
+              className={btn === "AC" || 
+              btn === "DEL" || 
+              btn === "/" || 
+              btn === "*" || 
+              btn === "-" || 
+              btn === "+" || 
+              btn === "="? "operator" : ""} />
+            );
+          })
+          }
+
       </ButtonBg>
     </Container>
   );
